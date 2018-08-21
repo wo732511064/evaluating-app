0932e1d774d551f691ebf3fc2c00b25a1fdf0a29
wo732511064 <732511064@qq.com>
2018-08-21 13:41:27 +0800

    huang

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,261 @@
+function append(parent, text) {
+    if (typeof text === 'string') {
+      var temp = document.createElement('div');
+      temp.innerHTML = text;
+      // 防止元素太多 进行提速
+      var frag = document.createDocumentFragment();
+      while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+      }
+      parent.appendChild(frag);
+    }
+    else {
+      parent.appendChild(text);
+    }
+  }
+function banner() {
+
+	//1 获取变量
+	// 屏幕的宽度
+	var width = document.body.offsetWidth;
+	// console.log(width);\
+
+	//  获取 轮播图的ul
+	var moveUl = document.querySelector('.banner_images');
+	var li1 = "<li></li>"
+	for(var j=1;j<=document.querySelectorAll(".banner_images li").length-1;j++){
+			append(document.querySelector(".banner_index-frame"), li1);
+		}
+	// 索引的li标签
+	var indexLiArr = document.querySelectorAll('.banner_index li');
+
+	// 定义 index 记录 当前的 索引值
+	// 默认 我们的ul 已经 往左边 移动了 一倍的宽度
+	var Ul = document.querySelector('.train_banner');
+	// (为什么 一位 最左边的图片 是用来做无限轮播的 不希望用户看到) 所以 index =1
+	var left = document.querySelector('.train_banner_left');
+	var right = document.querySelector('.train_banner_right');
+	var index = 0;
+	
+	
+	
+
+	// 抽取的代码 提升代码的可读性,以及 降低维护的难度
+	var startTransition = function() {
+		moveUl.style.transition = 'all .5s';
+	}
+
+	var endTransition = function() {
+		moveUl.style.transition = '';
+	}
+
+	// 由于 移动的距离 无法确定 所以提取为参数
+	var setTransform = function(distance) {
+		moveUl.style.transform = 'translateX(' + distance + 'px)';
+	}
+
+	// 开启定时器
+	var timeId = setInterval(function() {
+		// 累加
+		index++;
+		if(index >= document.querySelectorAll(".banner_images li").length) {
+						index = 0
+					}
+		// 将 过渡开启 管你三七二十一 只要进来 就开启过渡 保证 过渡效果一直存在
+		// moveUl.style.transition = 'all .3s';
+		startTransition();
+
+		// 修改 ul的位置
+		// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+		setTransform(index * width * -1);
+
+	}, 3000);
+
+	function li() {
+		for(var i = 0; i < indexLiArr.length; i++) {
+			indexLiArr[i].index = i;
+			indexLiArr[i].onclick = function() {
+				for(var i = 0; i < indexLiArr.length; i++) {
+					indexLiArr[i].className = '';
+				}
+				indexLiArr[this.index].className = 'current';
+				clearInterval(timeId);
+				endTransition();
+				index = this.index;
+				setTransform((this.index) * width * -1);
+				startTransition()
+
+				timeId = setInterval(function() {
+					// 累加
+
+					index++;
+					if(index >= document.querySelectorAll(".banner_images li").length) {
+						index = 0
+					}
+					// 将 过渡开启 管你三七二十一 只要进来 就开启过渡 保证 过渡效果一直存在
+					// moveUl.style.transition = 'all .5s';
+					startTransition();
+
+					// 修改 ul的位置
+					// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+					setTransform(index * width * -1);
+
+				}, 3000)
+			};
+
+		}
+	}
+	li();
+
+	//轮播左点击
+	left.addEventListener('click', function() {
+		clearInterval(timeId);
+		startTransition();
+		index--;
+		console.log(index)
+		if(index < 0) {
+			// 跳到倒数第二张
+			
+			index = document.querySelectorAll(".banner_images li").length-1;
+			// 关闭过渡
+			// moveUl.style.transition = '';
+
+			// 瞬间 修改一下 ul 的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+		} else {
+			setTransform(index * width * -1);
+		}
+
+		// 修改 索引li标签的 class
+		for(var i = 0; i < indexLiArr.length; i++) {
+			indexLiArr[i].className = '';
+		}
+
+		// 有一个 1的 差值
+		indexLiArr[index].className = 'current';
+
+		timeId = setInterval(function() {
+			// 累加
+			
+			index++;
+			if(index >= document.querySelectorAll(".banner_images li").length) {
+						index = 0
+					}
+			// 将 过渡开启 管你三七二十一 只要进来 就开启过渡 保证 过渡效果一直存在
+			// moveUl.style.transition = 'all .3s';
+			startTransition();
+
+			// 修改 ul的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+
+		}, 3000)
+
+	})
+
+	//轮播右点击
+	right.addEventListener('click', function() {
+		clearInterval(timeId);
+		startTransition();
+		index++;
+		if(index >= document.querySelectorAll(".banner_images li").length) {
+			// 跳到倒数第二张
+			index = 0;
+
+			// 关闭过渡
+			// moveUl.style.transition = '';
+
+			// 瞬间 修改一下 ul 的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+		} else{
+			setTransform(index * width * -1);
+		}
+
+		// 修改 索引li标签的 class
+		for(var i = 0; i < indexLiArr.length; i++) {
+			indexLiArr[i].className = '';
+		}
+
+		// 有一个 1的 差值
+		indexLiArr[index].className = 'current';
+
+		timeId = setInterval(function() {
+			// 累加
+
+			index++;
+			if(index >= document.querySelectorAll(".banner_images li").length) {
+						index = 0
+					}
+			// 将 过渡开启 管你三七二十一 只要进来 就开启过渡 保证 过渡效果一直存在
+			// moveUl.style.transition = 'all .3s';
+			startTransition();
+
+			// 修改 ul的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+
+		}, 3000)
+
+	})
+
+	// 过渡 结束事件 用来 修正 index的值 并修改索引
+	moveUl.addEventListener('webkitTransitionEnd', function() {
+
+		//  如果 index 太大了 
+		if(index >= document.querySelectorAll(".banner_images li").length) {
+			index = 0;
+
+			// 关闭过渡
+			// moveUl.style.transition = '';
+			endTransition();
+
+			// 瞬间 修改一下 ul 的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+		} else if(index < 0) {
+			// 跳到倒数第二张
+			index = document.querySelectorAll(".banner_images li").length-1;
+
+			// 关闭过渡
+			// moveUl.style.transition = '';
+			endTransition();
+
+			// 瞬间 修改一下 ul 的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+		}
+
+		// 修改 索引li标签的 class
+		for(var i = 0; i < indexLiArr.length; i++) {
+			indexLiArr[i].className = '';
+		}
+
+		// 有一个 1的 差值
+		indexLiArr[index].className = 'current';
+	})
+
+	window.onresize = function() {
+		endTransition();
+		clearInterval(timeId);
+		width = document.documentElement.clientWidth;
+		setTransform(index * width * -1);
+		timeId = setInterval(function() {
+			// 累加
+			index++;
+
+			// 将 过渡开启 管你三七二十一 只要进来 就开启过渡 保证 过渡效果一直存在
+			// moveUl.style.transition = 'all .3s';
+			startTransition();
+
+			// 修改 ul的位置
+			// moveUl.style.transform = 'translateX('+index*width*-1+'px)';
+			setTransform(index * width * -1);
+		}, 3000)
+	}
+
+}
 function TiMu(){
 	for(var i in data1){
 		var div = document.createElement("div");
@@ -114,36 +372,37 @@ function CountDown() {
  }
 
 window.onload = function(){
-	TiMu()
-	mintime = 1; 
-    timer = setInterval("CountDown()", 1000); 
-	var dact = document.querySelector(".entrance-bottom-frame-line")
-	var divli = document.querySelector(".entrance-bottom-frameli").children[0]
-	var active = "active"
-	var none = "none"
-	addClass(dact, active)
-	addClass(divli, active)
-	var timu_id = 0
-	var frame_left = 0
-	document.querySelector(".entrance-bottom-frame").style.marginLeft = frame_left + "%"
-	for(var i = 0;i<document.querySelectorAll(".entrance-bottom-frame-line-button").length;i++){
-		document.querySelectorAll(".entrance-bottom-frame-line-button")[i].onclick = function(){
-			if(timu_id < document.querySelectorAll(".entrance-bottom-frame-line").length - 1){
-				frame_left += -100
-				document.querySelector("#timer").style.opacity = 0
-				document.querySelector(".entrance-bottom-frame").style.marginLeft = frame_left + "%"
-				timu_id++;
-				addClass(document.querySelector(".entrance-bottom-frameli").children[timu_id], active)
-				addClass(document.querySelectorAll(".entrance-bottom-frame-line")[timu_id], active)
-				removeClass(document.querySelector(".entrance-bottom-frameli").children[timu_id-1], active)
-				removeClass(document.querySelectorAll(".entrance-bottom-frame-line")[timu_id-1], active)
-//				addClass(document.querySelectorAll(".entrance-bottom-frame-beijing")[timu_id-1],none)
-				setTimeout(function(){document.querySelector("#timer").style.opacity = 1},700)
-			}else{
-				window.location.href='result.html';
-			}
-		}
-	}
+	banner()
+//	TiMu()
+//	mintime = 1; 
+//  timer = setInterval("CountDown()", 1000); 
+//	var dact = document.querySelector(".entrance-bottom-frame-line")
+//	var divli = document.querySelector(".entrance-bottom-frameli").children[0]
+//	var active = "active"
+//	var none = "none"
+//	addClass(dact, active)
+//	addClass(divli, active)
+//	var timu_id = 0
+//	var frame_left = 0
+//	document.querySelector(".entrance-bottom-frame").style.marginLeft = frame_left + "%"
+//	for(var i = 0;i<document.querySelectorAll(".entrance-bottom-frame-line-button").length;i++){
+//		document.querySelectorAll(".entrance-bottom-frame-line-button")[i].onclick = function(){
+//			if(timu_id < document.querySelectorAll(".entrance-bottom-frame-line").length - 1){
+//				frame_left += -100
+//				document.querySelector("#timer").style.opacity = 0
+//				document.querySelector(".entrance-bottom-frame").style.marginLeft = frame_left + "%"
+//				timu_id++;
+//				addClass(document.querySelector(".entrance-bottom-frameli").children[timu_id], active)
+//				addClass(document.querySelectorAll(".entrance-bottom-frame-line")[timu_id], active)
+//				removeClass(document.querySelector(".entrance-bottom-frameli").children[timu_id-1], active)
+//				removeClass(document.querySelectorAll(".entrance-bottom-frame-line")[timu_id-1], active)
+////				addClass(document.querySelectorAll(".entrance-bottom-frame-beijing")[timu_id-1],none)
+//				setTimeout(function(){document.querySelector("#timer").style.opacity = 1},700)
+//			}else{
+//				window.location.href='result.html';
+//			}
+//		}
+//	}
 }
 
 
